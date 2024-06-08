2ef6324dd4c62ed241ed2f5a94aeb84bc589c949
Vitor Lobo <lobocode@gmail.com>
2024-06-08 16:53:53 -0300

    Enabled automatic formatting on save for clojure-mode

--- emacs/windows/custom/clojure.el
+++ emacs/windows/custom/clojure.el
@@ -17,12 +17,36 @@
 ;; Optional: for better REPL integration
 (use-package cider
   :ensure t
-  :after clojure-mode)
+  :after clojure-mode
+  :config
+  ;; Use cljfmt for formatting
+  (setq cider-format-code-options '(("cljfmt" ("--config" ".cljfmt.edn"))))
+  (add-hook 'cider-mode-hook #'cider-format-buffer))
 
 ;; Additional configuration for LSP (optional)
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode)
+
+;; Ensure `clj-refactor` is installed for better refactoring and formatting
+(use-package clj-refactor
+  :ensure t
+  :after clojure-mode
+  :config
+  (add-hook 'clojure-mode-hook (lambda ()
+                                 (clj-refactor-mode 1)
+                                 (yas-minor-mode 1) ; for adding namespace requires
+                                 (cljr-add-keybindings-with-prefix "C-c C-r"))))
+
+;; Enable auto-closing of brackets and double quotes
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (electric-pair-local-mode 1)))
+
+;; Automatically format code on save
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'cider-format-buffer nil 'local)))
 
 ;; If you manually installed clojure-lsp and it's not on your PATH
 ;; (setq lsp-clojure-server-command '("/path/to/clojure-lsp"))
