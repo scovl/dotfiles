7640c637719de86f4678e174c53678ec254998a1
Vitor Lobo <lobocode@gmail.com>
2025-11-21 20:47:43 -0300

    up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,14 +10,11 @@
       ./hardware-configuration.nix
     ];
 
-  
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "lobo"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.networkmanager.dns = "default";
 
@@ -29,11 +26,6 @@
   services.tumbler.enable = true;
   services.udisks2.enable = true;
   services.devmon.enable = true;
-
-  
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Layout do teclado no sistema todo (tty + X/Wayland)
   i18n.defaultLocale = "en_US.UTF-8";
@@ -51,9 +43,6 @@
     # NÃO definir console.keyMap aqui pra não dar conflito
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
   services.gnome.gnome-keyring.enable = true;
 
   programs.sway = {
@@ -61,8 +50,7 @@
     wrapperFeatures.gtk = true;
   };
 
-
- # XDG portals para sway + wayland
+  # XDG portals para sway + wayland
   services.dbus.enable = true;
 
   xdg.portal = {
@@ -71,15 +59,8 @@
     config.sway.default = lib.mkForce "wlr";
   };
 
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
 
   services.pipewire = {
     enable = lib.mkForce true;
@@ -89,7 +70,6 @@
     jack.enable = true;
   };
 
-
   # new filesystem
   fileSystems."/mnt/games" = {
     device = "/dev/sda2";
@@ -97,18 +77,14 @@
     options = [ "noatime" ];
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.lobo = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user. 
-     packages = with pkgs; [
-       tree
-     ];
-   };
-
+  users.users.lobo = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
 
   # flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -116,41 +92,80 @@
 
   programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  # ================================
+  # Ambiente global Elixir / Phoenix
+  # ================================
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     emacs
-     wget
-     fuzzel
-     firefox
-     alacritty
-     neofetch
-     grim
-     slurp
-     wl-clipboard
-     mako
-     pavucontrol
-     alsa-utils
-     gimp
-     blender
-     audacity
-     git
-     gcc
-     xfce.thunar
-     xfce.thunar-archive-plugin
-     file-roller
-     xfce.thunar-volman
-     p7zip
-     unzip
-     wf-recorder
-     ffmpeg
-     mangohud
-     gamemode
-     xdg-desktop-portal-wlr
-     xdg-desktop-portal-gtk
-     zoom-us
-   ];
+    vim
+    emacs
+    wget
+    fuzzel
+    firefox
+    alacritty
+    neofetch
+    grim
+    slurp
+    wl-clipboard
+    mako
+    pavucontrol
+    alsa-utils
+    gimp
+    blender
+    audacity
+    git
+    gcc
+    xfce.thunar
+    xfce.thunar-archive-plugin
+    file-roller
+    xfce.thunar-volman
+    p7zip
+    unzip
+    wf-recorder
+    ffmpeg
+    mangohud
+    gamemode
+    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gtk
+    zoom-us
+    yazi
+    (yazi.override {
+      _7zz = _7zz-rar;
+    })
+
+    # --- Elixir / Phoenix dev ---
+    elixir
+    erlang
+    nodejs_24
+    yarn
+    
+    # libs de build / utilitários
+    gnumake
+    openssl
+    zlib
+    libxml2
+    curl
+    libiconv
+    ];
+
+  # Variáveis de ambiente para mix/hex e Erlang
+  environment.variables = {
+    MIX_HOME   = "/home/lobo/.nix-mix";
+    HEX_HOME   = "/home/lobo/.nix-hex";
+
+    LANG       = "en_US.UTF-8";
+    ERL_AFLAGS = "-kernel shell_history enabled";
+    ERL_LIBS   = "/home/lobo/.nix-hex/lib/erlang/lib";
+  };
+
+  # Criar diretórios do mix/hex para o usuário lobo
+  system.activationScripts.elixirDirs = {
+    text = ''
+      HOME="/home/lobo"
+      mkdir -p "$HOME/.nix-mix" "$HOME/.nix-hex"
+      chown -R lobo:users "$HOME/.nix-mix" "$HOME/.nix-hex" || true
+    '';
+  };
+
 
   # Fonts
   fonts.packages = with pkgs; [
@@ -166,14 +181,8 @@
     powerline-fonts
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
+  # yazi
+  programs.yazi.enable = true;
 
   # Steam + Proton
   hardware.graphics.enable = true;
@@ -181,53 +190,14 @@
 
   programs.steam = {
     enable = true;
-
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-
-    };
+  };
 
   # Gamemode daemon
   programs.gamemode.enable = true;
 
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   networking.firewall.enable = true;
 
- 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
-
